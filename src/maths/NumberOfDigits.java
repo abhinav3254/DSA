b77b98d11f37c4f1fa59d453c8355edbab510934
abhinav3254 <abhinav3254@gmail.com>
2025-06-14 22:57:35 +0530

    calculate digits of base b

---
+++ src/maths/NumberOfDigits.java
@@ -0,0 +1,13 @@
+package maths;
+
+public class NumberOfDigits {
+
+    public static void main(String[] args) {
+        System.out.println(calDigits(16,2));
+    }
+
+    private static int calDigits(int n,int base) {
+        return (int) (Math.log(n)/Math.log(base)) + 1;
+    }
+
+}
